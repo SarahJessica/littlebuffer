ae85906415366296a0a83d784eddf1124816017d
Jessica Leivers <sarahjessicaleivers@gmail.com>
2015-05-29 16:24:50 -0700

    save edit buttons automagically toggling

--- frontend/app/posts.js
+++ frontend/app/posts.js
@@ -3,4 +3,13 @@ angular.module('app').controller('PostCtlr', function($scope, $http){
     console.info(posts);
     $scope.posts = posts.data;
   });
+  // $scope.post.edit = false;
+  $scope.makeUpdate = function(post){
+    post.edit = true;
+    console.log(post);
+  };
+  $scope.saveUpdate = function(post){
+    post.edit = false;
+  };
+  $scope.delete = function(post){};
 });
