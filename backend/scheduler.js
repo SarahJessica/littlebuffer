77befc03664b69ecabcd5e14d4143100403f1605
Jessica Leivers <sarahjessicaleivers@gmail.com>
2015-06-01 14:38:17 -0700

    tweets sending and deleting on send

--- backend/scheduler.js
+++ backend/scheduler.js
@@ -2,24 +2,6 @@ var sails = require('sails');
 var Twit = require('twit');
 var async = require('async');
 
-// async.series([
-//   function(callback){
-//     async.each(posts, function(post, cb){
-//       send tweet
-//       cb();
-//     }, callback)
-//   },
-//   function(callback){
-//     async.each(posts, function(post, cb){
-//       delete tweet
-//       cb();
-//     }, callback)
-//   }
-// ], function(){
-//   allDone;
-// })
-
-
 sails.load(function(){
   setInterval(function(){
     checkPosts();
@@ -32,27 +14,29 @@ function checkPosts(){
     datetime: {'<': new Date()}
     }).populate('owner').exec(function (err, posts){
     console.log(posts);
-    posts.forEach(function(post){
+    // posts.forEach(function(post){
       async.series([
         function(asyncCallback){
           async.each(posts, function(post, cb){
             sendTweet(post.message, function(){
-              destroyPostedPost(post);
+              // destroyPostedPost(post);
+              cb();
             });
-            cb();
-          }, syncCallback);
+          }, asyncCallback);
         },
-        function(syncCallback){
+        function(asyncCallback){
           async.each(posts, function(post, cb){
-            destroyPostedPost(post);
-            cb();
-          }, syncCallback);
+            destroyPostedPost(post, function(){
+              cb();
+            });
+            // cb();
+          }, asyncCallback);
         }
       ], function(){
         console.log('all done');
       });
       // sendTweet(post.owner.twitterToken, post.owner.twitterSecret, post.message, function);
-    });
+    // });
   });
 }
 // .populate is a waterline function that will/should bring in the twitter user data
@@ -74,12 +58,9 @@ function sendTweet(message, cb){
   });
 
 }
-function destroyPostedPost(post){
-  console.log('destroy being called');
+function destroyPostedPost(post, destroyCB){
+  Post.destroy({id:post.id}).exec(function deleteCB(err){
+    console.log('A sweet tweet delete');
+    destroyCB();
+  });
 }
-// function updatePostedPost(post){
-//   // post.isPosted = true;
-//   post.save(function(){
-//     console.log('isPosted changed to true');
-//   });
-// }
